70d53e6d1fc5692956b262e9856183776ffb53be
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-05-25 14:57:14 +0200

    Maj Streamlit

--- BackgroundRemoval-main/bg_remove.py
+++ BackgroundRemoval-main/bg_remove.py
@@ -6,11 +6,11 @@ import base64
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Remove background from your image")
+st.write("## Classez des images à l'aide d'algorithmes de Deep Learning")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog:Téléchargez une image de chien pour découvrir sa race. Des images de qualité complète peuvent être téléchargées à partir de la barre latérale. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
-st.sidebar.write("## Upload and download :gear:")
+st.sidebar.write("## Charger et télécharger :gear:")
 
 
 # Download the fixed image
@@ -23,18 +23,18 @@ def convert_image(img):
 
 def fix_image(upload):
     image = Image.open(upload)
-    col1.write("Original Image :camera:")
+    col1.write("Image Originale:camera:")
     col1.image(image)
 
     fixed = remove(image)
-    col2.write("Fixed Image :wrench:")
+    col2.write("la race:wrench:")
     col2.image(fixed)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+    st.sidebar.download_button("Télécharger l'image modiffiée", convert_image(fixed), "fixed.png", "image/png")
 
 
 col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+my_upload = st.sidebar.file_uploader("Télécharger une image", type=["png", "jpg", "jpeg"])
 
 if my_upload is not None:
     fix_image(upload=my_upload)
