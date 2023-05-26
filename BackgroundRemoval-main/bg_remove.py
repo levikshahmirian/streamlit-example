d86226d54f759c3a76cf999abcbc9ac75e9b5fb4
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-05-26 16:23:42 +0200

    u

--- BackgroundRemoval-main/bg_remove.py
+++ BackgroundRemoval-main/bg_remove.py
@@ -2,24 +2,14 @@ import streamlit as st
 from rembg import remove
 from PIL import Image
 from io import BytesIO
-import base6
-import pickle
+import base64
 
 
 import joblib
 
-pickled_model = pickle.load(open('model.pkl', 'rb'))
 
-#load saved model
-xgb = joblib.load('/app/streamlit-example/BackgroundRemoval-main/model.json')
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Classez des images à l'aide d'algorithmes de Deep Learning")
-st.write(
-    ":dog:Téléchargez une image de chien pour découvrir sa race. Des images de qualité complète peuvent être téléchargées à partir de la barre latérale. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
-)
-st.sidebar.write("## Charger et télécharger :gear:")
 
 
 # Download the fixed image
@@ -44,9 +34,26 @@ def fix_image(upload):
 
 col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("Télécharger une image", type=["png", "jpg", "jpeg"])
+my_upload1 = st.sidebar.file_uploader("Télécharger une Model", type=["json"])
 
 if my_upload is not None:
     fix_image(upload=my_upload)
 else:
     fix_image("/app/streamlit-example/BackgroundRemoval-main/zebra.jpg")
+
+if my_upload1 is not None:
+    #load saved model
+    xgb = joblib.load('my_upload1')
+
+    st.set_page_config(layout="wide", page_title="Image Background Remover")
+
+    st.write("## Classez des images à l'aide d'algorithmes de Deep Learning")
+    st.write(
+        ":dog:Téléchargez une image de chien pour découvrir sa race. Des images de qualité complète peuvent être téléchargées à partir de la barre latérale. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    )
+    st.sidebar.write("## Charger et télécharger :gear:")
+else:
+    fix_image("/app/streamlit-example/BackgroundRemoval-main/zebra.jpg")
+
+
 
