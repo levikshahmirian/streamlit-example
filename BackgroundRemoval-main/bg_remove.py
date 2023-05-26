0663d7cc87e9f2ca90e4afe35c051889e7facdca
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-05-26 17:11:19 +0200

    a

--- BackgroundRemoval-main/bg_remove.py
+++ BackgroundRemoval-main/bg_remove.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import base64
 import numpy
 import joblib
-import pickle 
+import pickle
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
@@ -34,14 +34,21 @@ def fix_image(upload):
     st.sidebar.download_button("Télécharger l'image modiffiée", convert_image(fixed), "fixed.png", "image/png")
 
 
+#col1, col2 = st.columns(2)
+#my_upload = st.sidebar.file_uploader("Télécharger une image", type=["png", "jpg", "jpeg"])
+
+#if my_upload is not None:
+    #fix_image(upload=my_upload)
+#else:
+    #fix_image("/app/streamlit-example/BackgroundRemoval-main/zebra.jpg")
+
 
 col1, col2 = st.columns(2)
 my_upload1 = st.sidebar.file_uploader("Télécharger une Model", type=["pkl"])
 
 if my_upload1 is not None:
     #load saved model
-    #xgb = joblib.load(my_upload1)
-    model = pickle.load(open(my_upload1,'rb'))
+    xgb = joblib.load(my_upload1)
     
 
 
