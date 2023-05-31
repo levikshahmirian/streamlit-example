96cce38484258b07e219c610647a8877a498b2dc
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-05-31 10:07:02 +0200

    e

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-import snowflake.connector
+
 import json
 from urllib.error import URLError
 from streamlit_autorefresh import st_autorefresh
@@ -18,7 +18,7 @@ def get_all_record_from_snowFlake():
     my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
     return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = ""
 my_data_rows= get_all_record_from_snowFlake()
 data = pd.DataFrame(my_data_rows)
 data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
