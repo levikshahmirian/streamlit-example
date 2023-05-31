795692a812eaa064e9c2e745316cae214c2b1d2a
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-05-31 10:20:43 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -13,34 +13,8 @@ st.set_page_config(page_title="Market Sales Dashboard", page_icon=":bar_chart:",
 
 
 # Function that fetch all data from snowflake table 
-def get_all_record_from_snowFlake():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
-    return my_cur.fetchall()
 
-my_cnx = ""
-my_data_rows= get_all_record_from_snowFlake()
-data = pd.DataFrame(my_data_rows)
-data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
-json_list = json.loads(data.to_json(orient='records'))
 
-data["hour"] = pd.to_datetime(data["Time"]).dt.hour
-data["hour"] = data["hour"].astype(int)
-
-data["Invoice ID"] =data["Invoice ID"].astype(str)
-data["Branch"]=data["Branch"].astype(str)
-data["City"]=data["City"].astype(str)
-data["Customer_type"]=data["Customer_type"].astype(str)
-data["Gender"]=data["Gender"].astype(str)
-data["Product line"]=data["Product line"].astype(str)
-data["Unit price"]=data["Unit price"].astype(float)
-data["Quantity"]=data["Quantity"].astype(int)
-data["Tax 5%"]=data["Tax 5%"].astype(float)
-data["Total"]=data["Total"].astype(float)
-data["Payment"]=data["Payment"].astype(str)
-data["cogs"]=data["cogs"].astype(float)
-data["gross margin percentage"]=data["gross margin percentage"].astype(float)
-data["Rating"]=data["Rating"].astype(float)
 
 
 st.sidebar.image(
@@ -61,24 +35,17 @@ span[data-baseweb="tag"] {
 )
 city = st.sidebar.multiselect(
     "Select the City:",
-    options=data["City"].unique(),
-    default=data["City"].unique(),
+
 )
 
 customer_type = st.sidebar.multiselect(
     "Select the Customer Type:",
-    options=data["Customer_type"].unique(),
-    default=data["Customer_type"].unique(),
+
 )
 
 gender = st.sidebar.multiselect(
     "Select the Gender:",
-    options=data["Gender"].unique(),
-    default=data["Gender"].unique()
-)
 
-df_selection = data.query(
-    "City == @city & Customer_type == @customer_type & Gender == @gender"
 )
 
 
@@ -86,61 +53,38 @@ df_selection = data.query(
 st.title(":bar_chart: Market Sales Dashboard")
 st.markdown("##")
 
-# TOP KPI's
-
-total_sales = int(df_selection["Total"].astype(float).sum())
-average_rating = round(df_selection["Rating"].astype(float).mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0)) 
-
-average_sale_by_transaction = round(df_selection["Total"].astype(float).mean(), 2)
-
-left_column, middle_column, right_column = st.columns(3)
-with left_column:
-    st.subheader("Total Sales:")
-    st.subheader(f"US $ {total_sales:,}")
-with middle_column:
-    st.subheader("Average Rating:")
-    st.subheader(f"{average_rating} {star_rating}")
-with right_column:
-    st.subheader("Average Sales Per Transaction:")
-    st.subheader(f"US $ {average_sale_by_transaction}")
 
 
 st.markdown("##")
 
 
-df = st.dataframe(data)
 
 st.markdown("""---""")
 
-new_groupe = df_selection.groupby(by=["Product line"])
+
 
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = (
-     new_groupe.aggregate(np.sum)[["Total"]].sort_values(by="Total")
+    
 )
 
 
 # SALES BY HOUR [BAR CHART]
-sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
+
 
 
 
 left_column, right_column = st.columns(2)
 
 
-# SALES BY HOUR [line]
-sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
 
-
-sales_per_city = df_selection.groupby(by=["City"]).sum()[["Total"]]
 
 
 
 
 with left_column:
     st.subheader("Filtred Data:")
-df = st.dataframe(data)
+
 
 pd.set_option('display.max_rows', None)  # Display all rows
 pd.set_option('display.max_columns', None)  # Display all columns
