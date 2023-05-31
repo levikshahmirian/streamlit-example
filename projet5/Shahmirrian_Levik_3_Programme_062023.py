e175430f26d6ce41502d8bc5bbc0baed181fac5c
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-05-31 09:36:28 +0200

    d

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.set_page_config(page_title="NYC Venue Search", layout="wide", initial_sidebar_state="expanded")
 
 # UI text strings 
-page_title = "NYC Venue Search"
-page_helper = "Discover NYC! The Streamlit app uses cosine similarity to semantically match your query with Foursquare venue categories and find matching venues in your selected areas."
+page_title = "FormationML Projet 5"
+page_helper = "Poser votre question "
 empty_search_helper = "Select a borough and neighborhood, and enter a search term to get started."
 category_list_header = "Suggested venue categories"
 borough_search_header = "Select a borough"
@@ -18,7 +18,18 @@ venue_list_header = "Venue details"
 
 
 
-
+def render_search_result():
+    """
+    Render the search results on the main content area.
+    """
+    col1, col2 = st.columns([1,2])
+    col1.write(category_list_header)
+    col1.table(st.session_state.suggested_categories)
+    col2.write(f"Found {len(st.session_state.suggested_places)} venues.")
+    if (len(st.session_state.suggested_places) > 0):
+        col2.map(st.session_state.suggested_places, zoom=13, use_container_width=True)
+        st.write(venue_list_header)
+        st.dataframe(data=st.session_state.suggested_places, use_container_width=True)
 
 
 boroughs = [{'NAME':'Brooklyn'},{'NAME':'Bronx'},{'NAME':'Manhattan'},{'NAME':'Queens'},{'NAME':'Staten Island'}]
