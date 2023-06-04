5f55096d6e4702eb89f4b1030bcc2c246b24ca26
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-04 22:54:35 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -13,62 +13,8 @@ st.set_page_config(page_title="Poser votre question", layout="wide")
 
 st.sidebar.header("Choisissez les tags:")
 init_options = [" "]
-tags_list = [""]
-multise_tags = st.sidebar.multiselect(
-    'Choisissez dans la liste :',            
-    options = tags_list
-    )
 
 
-
-def write_predict():
-    st.session_state["submit"]=True
-
-def load_model(img):
-    json_file = open('/app/streamlit-example/projet5/model_num.json', 'r')
-
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-
-    # load weights into new model
-    loaded_model.load_weights("/app/projet5/Formation_ML/model_num.h5")
-    #print("Loaded model from disk")
-
-
-# ---- MAINPAGE ----
-st.title("Formation_ML Projet 5 ")
-st.markdown("##")
-
-query_body = st.text_area("Ask a question about the document")
-query_title = st.text_input(label="Topic (or hashtag)", placeholder="Title", on_change= write_predict())
-
-doc = None
-
-
-#button = st.button("Choisir des Tags")
-if  st.session_state.get("submit"):
-
-    if not query_title:
-        st.error("Donnez un titre à votre question!")
-    else:
-        tags_list=query_title.split(" ")
-        multise_tags.insert(1, tags_list)
-
-if len(multise_tags) > 0:       
-    st.title(multise_tags)
-
-if st.button('Enregistrer'):
-    if not query_body:
-        st.error("Saisissez votre question!")
-    elif not query_title:
-        st.error("Donnez un titre à votre question!")
-    else:
-        st.write("Votre question est enregistrée !")
-
-
-
-init_options = query_title
 
 if 'options' not in st.session_state:
     st.session_state.options = init_options
