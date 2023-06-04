1fee28491e37cc5fd66b379ef9f28bd819c916a3
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-04 11:38:18 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -62,8 +62,11 @@ if button or st.session_state.get("submit"):
         st.error("Please enter a question!")
     else:
         tags_list = query_title + query_body
-        tags_suggestion
+        st.sidebar.multiselect(
+            "Selectionnez des tags:",
+            tags_list
+        )
         st.session_state["submit"] = True
 
-st_autorefresh(interval=200, key="dataframe")
+#st_autorefresh(interval=20000, limit=100, key="dataframe")
 
