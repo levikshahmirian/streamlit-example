2a1e50cdc662cc183ceacbe202512383c17fdb5e
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-04 12:04:46 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -67,11 +67,9 @@ if button or st.session_state.get("submit"):
 
 #st_autorefresh(interval=20000, limit=100, key="dataframe")
 
-txt = st.text_area('Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
-    ''')
-st.write('Sentiment:', write_predict(txt))
+options = st.multiselect(
+    'What are your favorite colors',
+    tags_list
+    )
+
+st.write('You selected:', options)
