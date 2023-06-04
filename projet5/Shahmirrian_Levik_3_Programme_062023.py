f671b187d054c3ce0b0f5d6c2f299a030c2d0928
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-04 11:54:22 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -67,33 +67,11 @@ if button or st.session_state.get("submit"):
 
 #st_autorefresh(interval=20000, limit=100, key="dataframe")
 
-# Store the initial value of widgets in session state
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.checkbox("Disable text input widget", key="disabled")
-    st.radio(
-        "Set text input label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
-    st.text_input(
-        "Placeholder for the other text input widget",
-        "This is a placeholder",
-        key="placeholder",
-    )
-
-with col2:
-    text_input = st.text_input(
-        "Enter some text 👇",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
-
-    if text_input:
-        st.write("You entered: ", text_input)
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+st.write('Sentiment:', write_predict(txt))
