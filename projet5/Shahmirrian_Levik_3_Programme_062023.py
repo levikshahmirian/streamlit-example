6d7398d97a18ea4528512376bdaf4f112dff9bf3
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-06 22:53:00 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -6,6 +6,7 @@ import numpy as np
 from keras.models import model_from_json
 from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input, decode_predictions
+import pickle
 
 st.set_page_config(page_title="Poser votre question", layout="wide")
 st.sidebar.header("Choisissez les tags:")
@@ -24,18 +25,12 @@ query_body = st.text_area("Ask a question about the document")
 query_title = st.text_input(label="Topic (or hashtag)", placeholder="Title", on_change= tags_list_change())
 
 def load_model(img):
-    json_file = open('/app/streamlit-example/Formation_ML/model_num.json', 'r')
 
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-
-    # load weights into new model
-    loaded_model.load_weights("/app/streamlit-example/Formation_ML/model_num.h5")
-    #print("Loaded model from disk")
+    # load model into new model
+    pickled_model = pickle.load(open('model.pkl', 'rb'))
 
     x = preprocess_input(np.expand_dims(img.copy(), axis=0))
-    preds = loaded_model.predict(x)
+    preds = pickled_model.predict(x)
 
 query_title = query_title.split(' ') 
 st.session_state.options = query_title
