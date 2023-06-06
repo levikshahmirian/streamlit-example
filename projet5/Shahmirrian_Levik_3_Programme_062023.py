05d7dba33b179ba8244b404fe1450ba5961487ff
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-06 21:11:52 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -23,6 +23,7 @@ def tags_list_change():
 query_body = st.text_area("Ask a question about the document")
 query_title = st.text_input(label="Topic (or hashtag)", placeholder="Title", on_change= tags_list_change())
 
+query_title = query_title.split(' ') 
 st.session_state.options = query_title
 
 if 'options' not in st.session_state:
