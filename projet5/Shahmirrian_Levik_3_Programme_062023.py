5e9370fcd8a2455e59e398dce69ef550644d581a
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-06 21:34:36 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-from streamlit_autorefresh import st_autorefresh
 import json
 import numpy as np 
 from keras.models import model_from_json
@@ -20,8 +19,23 @@ st.markdown("##")
 def tags_list_change():
     init_options = tags_list
 
+
 query_body = st.text_area("Ask a question about the document")
 query_title = st.text_input(label="Topic (or hashtag)", placeholder="Title", on_change= tags_list_change())
+
+def load_model(img):
+    json_file = open('/app/streamlit-example/Formation_ML/model_num.json', 'r')
+
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+
+    # load weights into new model
+    loaded_model.load_weights("/app/streamlit-example/Formation_ML/model_num.h5")
+    #print("Loaded model from disk")
+
+    x = preprocess_input(np.expand_dims(img.copy(), axis=0))
+    preds = loaded_model.predict(x)
 
 query_title = query_title.split(' ') 
 st.session_state.options = query_title
