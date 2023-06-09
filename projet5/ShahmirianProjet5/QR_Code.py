8a79b85c58e1b5e75d57afcc583b14d51e8d6edb
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-09 15:58:45 +0200

    a

--- projet5/ShahmirianProjet5/QR_Code.py
+++ projet5/ShahmirianProjet5/QR_Code.py
@@ -29,7 +29,7 @@ def tags_list_change():
 def lemmatize(text):
 	sp.cli.download("en_core_web_sm")
 	nlp = sp.load("en_core_web_sm")
-	doc = nlp(text)
+	doc = nlp(text.lower())
 	tokens = [token.lemma_ for token in doc if not (token.is_stop or token.is_punct or len(token) == 1 or token.is_digit or token.like_url)]
 	return ' '.join(tokens)
 
@@ -56,7 +56,7 @@ def lowercase(text):
 
 def clean_text(text):
     _steps = [
-    RemoveHTMLTags,
+    #RemoveHTMLTags,
     #lowercase,
     #Expand_the_Contractions,
     lemmatize
