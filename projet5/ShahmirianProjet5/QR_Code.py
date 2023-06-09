0e78ec66deb60787d1ee35c90c75c93fd58bd5f9
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-09 16:49:43 +0200

    a

--- projet5/ShahmirianProjet5/QR_Code.py
+++ projet5/ShahmirianProjet5/QR_Code.py
@@ -39,7 +39,7 @@ def RemoveHTMLTags(text):
     for char in char_remov:
         # replace() "returns" an altered string
         text = text.replace(char, " ")
-    return text.split(" ").tolist()
+    return text.split(" ")
 
 def lowercase(text):
 
