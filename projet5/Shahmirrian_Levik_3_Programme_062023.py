f5b8272e3537468a9717a622b7a3618134e203b6
levikshahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-11 10:51:29 +0200

    a

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
-import numpy as np 
-from keras.models import model_from_json
+import numpy as np
+import os
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
 from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input, decode_predictions
 import pickle
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import contractions
@@ -15,57 +17,28 @@ import spacy as sp
 import sklearn
 import functools
 
-
-
-st.set_page_config(page_title="Poser votre question", layout="wide")
-st.sidebar.header("Choisissez les tags:")
-init_options = [""]
-tags_list = [""]
-
-sp.cli.download("en_core_web_sm")
-nlp = sp.load("en_core_web_sm")
-
-# ---- MAINPAGE ----
-st.title("Formation_ML Projet 5 ")
-st.markdown("##")
-
-def tags_list_change():
-    init_options = tags_list
+st.set_page_config(page_title="Poser votre question",)
+init_options = [" "]
 
 #applique la lemmatization et enlève les StopWords, des mots de longeurs 1, et les chiffres """
 def lemmatize(text):
-   
-   doc = nlp(text)
-   tokens = [token.lemma_ for token in doc if not (token.is_stop or token.is_punct or len(token) == 1 or token.is_digit or token.like_url)]
-   
-   return ' '.join(tokens)
+	sp.cli.download("en_core_web_sm")
+	nlp = sp.load("en_core_web_sm")
+	doc = nlp(text)
+	tokens = [token.lemma_ for token in doc if not (token.is_stop or token.is_punct or len(token) == 1 or token.is_digit or token.like_url)]
+	return ' '.join(tokens)
 
+def removeSpecialChar(text):
+    char_remov = ['"', '(', ')', "'", '[', ']']
+    for char in char_remov:
+        # replace() "returns" an altered string
+        clean_text = text.replace(char, " ")
+    return str(clean_text)
 
-def RemoveHTMLTags(text):
-    tag = False
-    quote = False
-    out = ""
-
-    for c in text:
-            if c == '<' and not quote:
-                tag = True
-            elif c == '>' and not quote:
-                tag = False
-            elif (c == '"' or c == "'") and tag:
-                quote = not quote
-            elif not tag:
-                out = out + c
-
-    return out
-
-def lowercase(text):
-
-    return text.lower()
 
 def clean_text(text):
     _steps = [
-    RemoveHTMLTags,
-    lowercase,
+    removeSpecialChar,
     #Expand_the_Contractions,
     lemmatize
     ]
@@ -73,56 +46,45 @@ def clean_text(text):
         text=step(text)
     return text   
 
-
-def load_apply_model(clean_text):
-
+def load_apply_model(text):
+    #st.write(text)
     # load model into new model
-    pickled_model = pickle.load(open('/app/streamlit-example/projet5/model.pkl', 'rb'))
-
-    #x = clean_text(query_title).split(' ') 
-    #preds = pickled_model.predict(x)
-    st.write(str(query_title))
-
-query_body = st.text_area("Ask a question about the document")
-query_title = st.text_input(label="Topic (or hashtag)", placeholder="Title", on_change= tags_list_change())
-#Développer les contractions"""
-#def Expand_the_Contractions(text):
-    #return contractions.fix(text)
-
-
-
+    pickled_model = pickle.load(open('projet5/ShahmirianProjet5/model_projet5.pkl', 'rb'))
+    pickled_vectorizer = pickle.load(open('projet5/ShahmirianProjet5/vectorizer.pickle', 'rb'))
     
+    corpora_Lemm_Title = clean_text(text).split()
+    x = pickled_vectorizer.transform(corpora_Lemm_Title)
+    feature_names_Title = pickled_vectorizer.get_feature_names_out()
 
-#init_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #sorted_items=sort_coo(tf_idf_vector.tocoo())
 
-
-if 'options' not in st.session_state:
-    st.session_state.options = init_options
-if 'default' not in st.session_state:
-    st.session_state.default = []
-
-
-ms = st.sidebar.multiselect(
-    label='Pick a number',
-    options=st.session_state.options,
-    default=st.session_state.default
-)
-
-# If 1 is selected, remove the 2 and rerun.
-if 1 in ms:
-    if 2 in st.session_state.options:
-        st.session_state.options.remove(2)
-        st.session_state.options.append("Salut")
-        st.session_state.default = ms
-        st.experimental_rerun()
-
-# Else if 2 is selected, remove the 1 and rerun.
-elif 2 in ms:
-    if 1 in st.session_state.options:
-        st.session_state.options.remove(1)
-        st.session_state.default = ms
-        st.experimental_rerun()
+    #x = clean_text(text).split()
+    preds = pickled_model.predict(x)
+    st.write(preds)
+    return feature_names_Title
 
 
-st.write('##### Valid Selection')
-st.write(str(ms))
+st.subheader("Poser votre question")
+
+body_text = st.text_area("Détailler votre question)")
+title_text = st.text_input("Donner un titre à votre question",autocomplete=" ", key="user_title_text")
+
+if "user_title_text" in st.session_state and len(st.session_state.user_title_text) > 0 :
+     #st.write(title_text)
+     if 'options' not in st.session_state:
+        st.session_state.options = init_options
+
+     if 'default' not in st.session_state:
+        st.session_state.default = []
+
+     ms = st.multiselect(
+        label='Pick a number', key="tags_selection",
+        options=load_apply_model(title_text),
+        default=st.session_state.default
+    )    
+else:
+    st.warning("Saisissez votre question et valider pour obtenir des tags.")
+
+submit_button = st.button("Enregistrer")
+if submit_button :
+	st.write("Votre question est enregistrée")
