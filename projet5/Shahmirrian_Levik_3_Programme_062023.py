4a71f3db92f32b77e8b6dfc56a78a0f8ec023883
Shahmirian <131209802+levikshahmirian@users.noreply.github.com>
2023-06-16 15:48:48 +0200

    Update Shahmirrian_Levik_3_Programme_062023.py

--- projet5/Shahmirrian_Levik_3_Programme_062023.py
+++ projet5/Shahmirrian_Levik_3_Programme_062023.py
@@ -24,6 +24,9 @@ init_options = [" "]
 st.sidebar.write("## Charger le modèle :gear:")
 my_upload = st.sidebar.file_uploader("Télécharger une image", type=["pickle", "pkl", "zip"])
 
+st.sidebar.write("## Charger le vectorizer :gear:")
+my_upload = st.sidebar.file_uploader("Télécharger une image", type=["pickle", "pkl", "zip"])
+
 if my_upload is not None:
     #fix_image(upload=my_upload)
 	
